b85d63ea6500140412c9a28e449efd8bb44b7039
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-05-02 17:50:33 +0200

    get data from api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,39 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const HomePage = () => {
+  const [events, setEvents] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('https://sonicscore-api-default-rtdb.europe-west1.firebasedatabase.app/events.json')
+      .then((response) => {
+        const eventsArr = Object.keys(response.data).map((id) => ({
+          id,
+          ...response.data[id],
+        }));
+        setEvents(eventsArr);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (!events) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <h1>HomePage</h1>
+      <div>
+        {events.map((event) => (
+          <div className="card" key={event.id}>
+            <p>{event.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
