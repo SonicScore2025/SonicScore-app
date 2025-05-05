2af5f50ba6021b46ca8bc1d074c31e1905510fd5
Pedram Ghane <p.ghane@gmail.com>
2025-05-05 16:09:20 +0200

    Merge with Andre Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import HomePage from "./pages/HomePage";
+import EventDetailsPage from "./pages/EventDetailsPage";
+import CreateReview from "./pages/CreateReview";
+import EditReview from "./pages/EditReview";
+
 import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 import AdminDashboardPage from "./pages/AdminDashboardPage";
 import AdminEventsListPage from "./pages/AdminEventListPage";
 import AdminCreateEventPage from "./pages/AdminCreateEventPage";
 import AdminUpdateEventPage from "./pages/AdminUpdateEventPage";
 import AdminRatingsListPage from "./pages/AdminRatingsListPage";
-import EventDetailsPage from "./pages/EventDetailsPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import AdminDashboardPage from './pages/AdminDashboardPage';
-import CreateEventPage from './pages/CreateEventPage';
-import UpdateEventPage from './pages/UpdateEventPage';
-import EventDetailsPage from './pages/EventDetailsPage';
-import CreateReview from './pages/CreateReview';
-import EditReview from './pages/EditReview';
 
 function App() {
   return (
@@ -37,8 +32,14 @@ function App() {
 
           <Route path="/admin" element={<AdminDashboardPage />} />
           <Route path="/admin/events" element={<AdminEventsListPage />} />
-          <Route path="/admin/events/create" element={<AdminCreateEventPage />} />
-          <Route path="/admin/event/:id/update" element={<AdminUpdateEventPage />} />
+          <Route
+            path="/admin/events/create"
+            element={<AdminCreateEventPage />}
+          />
+          <Route
+            path="/admin/event/:id/update"
+            element={<AdminUpdateEventPage />}
+          />
           <Route path="/admin/ratings" element={<AdminRatingsListPage />} />
 
           <Route path="*" element={<NotFoundPage />} />
